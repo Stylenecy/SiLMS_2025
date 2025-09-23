14f3055ee37faaacdc3d062d7f50b6245cbef092
claraanjelitakd <claraanjelitakd@gmail.com>
2025-09-23 17:02:44 +0700

    Python

---
+++ 72230663.py
@@ -0,0 +1,27 @@
+def tambah(a,b):
+    return a+b
+def kurang(a,b):
+    return a-b
+def bagi(a,b):
+    return a/b
+def kali(a,b):
+    return a*b
+
+print('Hasil + -->', tambah(5,4), '\n')
+print('Hasil - -->', kurang(5,4), '\n')
+print('Hasil / -->', bagi(5,4), '\n')
+print('Hasil * -->', kali(5,4), '\n')
+
+def ubah(x):
+    if x == 'a':
+        print('1')
+    elif x == 'b':
+        print('2')
+    elif x == 'c':
+        print('3')
+    elif x == 'd':
+        print('4')
+    elif x == 'e':
+        print('5')
+
+print(ubah('c'))
